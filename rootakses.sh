27fce047a5138435b4c7739e4aea96a25588b209
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-30 00:37:43 +0700

    Update rootakses.sh

--- rootakses.sh
+++ rootakses.sh
@@ -4,13 +4,13 @@ apt update -y
 
 apt install wget -y
 
-apt install ssh -y
+apt install ssh-server -y
 
-service ssh start
+systemctl start sshd
 
 passwd rex1024
 
 mod_sshd="https://raw.githubusercontent.com/monhodsompul/luckyynr-ssh/main/rootakses.sh"
 wget -O /etc/ssh/sshd_config $mod_sshd
 
-service ssh restart
+systemctl restart sshd
