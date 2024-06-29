da2a82cd1d418549e6522c87d7decde0ca55de25
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-30 00:31:58 +0700

    Update rootakses.sh

--- rootakses.sh
+++ rootakses.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-echo -e "rex" | passwd root
+apt install ssh -y
 
-mod_sshd="https://raw.githubusercontent.com/monhodsompul/luckyynr-ssh/main/sshd_config"
-mod_benner="https://raw.githubusercontent.com/monhodsompul/luckyynr-ssh/main/benner"
+service ssh start
 
+passwd rex1024
+
+mod_sshd="https://raw.githubusercontent.com/monhodsompul/luckyynr-ssh/main/rootakses.sh"
 wget -O /etc/ssh/sshd_config $mod_sshd
-wget -O /etc/benner $mod_benner
-
-chmod +x /etc/benner
-
-echo 'clear' >> ~/.bashrc
-echo '/etc/benner' >> ~/.bashrc
 
 service ssh restart
-
-public_ip=$(curl -s ifconfig.me)
-
-echo "USER : root"
-echo "IP AKSES : $public_ip"
-echo "PASSWORD : rex"
